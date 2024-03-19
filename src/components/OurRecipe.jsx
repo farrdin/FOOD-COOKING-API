7a0ddc54665c8a07d50d36797c916f3661dee9e4
Fardin Ahmed Alif <Alif.Ahmed44@gmail.com>
2024-03-20 02:27:01 +0600

    changes

--- src/components/OurRecipe.jsx
+++ src/components/OurRecipe.jsx
@@ -2,7 +2,7 @@ const OurRecipe = () => {
   return (
     <div>
       {" "}
-      <div className="mt-36 mb-20 w-[62%] mx-auto ">
+      <div className="mt-28 mb-20 w-[62%] mx-auto ">
         <h1 className="text-center text-4xl font-semibold text-[#150B2B] mb-10">
           Our Recipes
         </h1>
